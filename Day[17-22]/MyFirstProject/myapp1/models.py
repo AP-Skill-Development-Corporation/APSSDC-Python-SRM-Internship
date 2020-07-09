d3dcedf2190b655bf01a10e4267610e9639fbe3b
N. Ravi Kumar[NRK] <rravikumar34@gmail.com>
2020-07-09 18:39:13 +0530

    Add files via upload

---
+++ Day[17-22]/MyFirstProject/myapp1/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class UsReg(models.Model):
+	username = models.CharField(max_length=150)
+	password = models.CharField(max_length=50)
+	salary = models.FloatField()
+	age = models.IntegerField()
+
+	def __str__(self):
+		return self.username+" "+str(self.age)
+
